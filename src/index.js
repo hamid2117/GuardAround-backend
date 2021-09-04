0368d29fa8d1cc47b4ccb3873b0f389323f5476d
hamid2117 <saim008865@gmail.com>
2021-09-04 14:14:57 +0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 // const app = require('./app')
 import app from './app.js'
 
-const port = 'https://guardaround.herokuapp.com/'
-// const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
+
 app.listen(port, () => {
-  console.log(
-    `Server running in https://guardaround.herokuapp.com/ : https://guardaround.herokuapp.com/`
-  )
+  console.log(`Server running in  : https://guardaround.herokuapp.com/`)
 })
